6639bf4fe107beeda2b4854234b70ad418ac7af4
Miguel <alejandrogroso@hotmail.com>
2014-02-28 18:31:20 -0200

    Create system.h

---
+++ system.h
@@ -0,0 +1,11 @@
+/*																													*
+*									"Cabecera de identificacion de sistemas"												*
+*																													*/
+#ifndef __system_h
+#define __system_h
+
+//#define __WIN32__			//Si compilas para windows..
+#define __LINUX__ 			//Si compilas para linux..
+/*	Descomenta solo la linea que corresponda	*/
+
+#endif
