879af82281ef18f38591612ce8cc1a72b4750ae8
Miguel <alejandrogroso@hotmail.com>
2014-02-28 18:35:27 -0200

    Create char-defined.h

---
+++ char-defined.h
@@ -0,0 +1,29 @@
+/*																													*
+*										"Cabecera de definiciones varias"											*
+*																													*/
+
+#ifndef __char_defined_h
+#define __char_defined_h
+
+
+#define Numero '0'
+#define BASE Numero
+
+#define Punto '.'
+#define Fraccion ','
+#define Coma Fraccion
+#define TwoPnt ':'
+#define Espacio ' '
+
+#define Guionmedio '-'
+#define SNeg Guionmedio
+#define OpAdicion Guionmedio
+#define Adicion '+'
+#define SPos Adicion
+
+#define Tab '\t'
+#define Tabvertical '\v'
+#define Newline '\n'
+
+
+#endif
